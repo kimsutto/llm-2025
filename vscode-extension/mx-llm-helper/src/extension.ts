44a7bb7249fd3f6d5f61f978721612bddd913115
Stella Yu <stellashyou@gmail.com>
2025-05-14 23:01:15 +0900

    보일러플레이트 생성

--- vscode-extension/mx-llm-helper/src/extension.ts
+++ vscode-extension/mx-llm-helper/src/extension.ts
@@ -1,22 +1,26 @@
-import * as vscode from "vscode";
-import { askFile } from "./commands/askFile";
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
 
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log("mx-llm-helper activated");
 
-  // 기존 helloWorld
-  context.subscriptions.push(
-    vscode.commands.registerCommand("mx-llm-helper.helloWorld", () => {
-      vscode.window.showInformationMessage("Hello World from mx-llm-helper!");
-    })
-  );
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "mx-llm-helper" is now active!');
 
-  // 새 askFile
-  context.subscriptions.push(
-    vscode.commands.registerCommand("mx-llm-helper.askFile", () =>
-      askFile(context)
-    )
-  );
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('mx-llm-helper.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from mx-llm-helper!');
+	});
+
+	context.subscriptions.push(disposable);
 }
 
+// This method is called when your extension is deactivated
 export function deactivate() {}
